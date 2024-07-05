70b3811df49e12274b74629553b25df541868b70
SantiagoPerezV <138243132+SantiagoPerezV@users.noreply.github.com>
2024-07-05 15:32:41 -0300

    Add files via upload

--- PerezVilla.Santiago.A221/SPL2_1C2024-main/SPLII/Clases/Barco.cs
+++ PerezVilla.Santiago.A221/SPL2_1C2024-main/SPLII/Clases/Barco.cs
@@ -1,7 +1,63 @@
-﻿namespace Clases
+﻿using System.Text;
+
+namespace Clases
 {
-    public class Class1
+    public abstract class Barco
     {
+        public enum EOperacion
+        {
+            Reparar_Mastil,
+            Pintar,
+            Cambiar_Velas,
+            Reparar_Mascaron,
+            Reparar_Casco,
+            Recargar_Cañones
+        }
+
+        protected float costo;
+        protected bool estadoReparado;
+        protected string nombre;
+        protected EOperacion operacion;
+        protected int tripulacion;
+
+        public float Costo { get => costo;}
+        public bool EstadoReparado { get => estadoReparado;}
+        public string Nombre { get => nombre;}
+        public EOperacion Operacion { get => operacion;}
+        protected int Tripulacion { get => tripulacion;}
+
+        public Barco()
+        {
+            
+        }
+
+        protected abstract void CalcularCosto();
+
+        public bool CompararBarcos(Barco a, Barco b)
+        {
+            bool retorno = false;
+            if (a.Nombre == b.Nombre)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        //public static bool operator ==(Barco a, Barco b)
+        //{
+        //    return a.Nombre == b.Nombre;
+        //}
+        //public static bool operator !=(Barco a, Barco b)
+        //{
+        //    return !(a == b);
+        //}
+
+        public override string ToString()
+        {
+            StringBuilder cadena = new StringBuilder();
+            cadena.AppendLine($"{Nombre} -- {Costo} -- {EstadoReparado} -- {Operacion} -- {Tripulacion}");
+            return cadena.ToString();
+        }
 
     }
 }
